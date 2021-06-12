85636657968f78915b236573893b4a1d7f5f70b0
Pro465 <67726964+Pro465@users.noreply.github.com>
2021-06-12 11:24:36 +0530

    Update main.rs
    added description

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-//rust 1.30.0 
+//==driver code==
+
 mod board;
 use board::Board;
 
 fn main() {
   let mut b = Board::new();
   println!("Welcome to my Tic Tac Toe In Rust😀");
-  
-  println!("This is my rust+OOP version of my JS+Functional Programming attempt in making a Tic Tac Toe AI");
   
   b.print_board();
   
